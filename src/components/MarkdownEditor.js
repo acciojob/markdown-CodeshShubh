0d8cddbb411ae97ac8fc2dbb338fccfb97f6d2bb
CodeshShubh <codeshshubh@gmail.com>
2025-04-02 10:48:43 +0530

    completed

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {marked} from 'marked';
 
 const MarkdownEditor = ({markdownInput}) => {
-    const [input, setinput] = useState('');
+    const [input, setinput] = useState(markdownInput);
     const [isLoading , setisLoading] = useState(false)
     useEffect(()=>{
         setisLoading(true);
@@ -10,15 +10,15 @@ const MarkdownEditor = ({markdownInput}) => {
             setinput(markdownInput)
             setisLoading(false)
          },500)
-         return ()=>{
-             
-       clearInterval(vaule)
-         }
+         return ()=>clearInterval(vaule)
+         
     },[markdownInput])
+
+  
   return (
     <div style={{border:'1px solid red', width:'50%'}} className='preview'>
  {
-    isLoading ? ( <p>Loading.....</p> ) : (<h1 dangerouslySetInnerHTML={{__html:marked(input)}} ></h1>)
+    isLoading ? ( <p>Loading.....</p> ) : <div dangerouslySetInnerHTML={{ __html: marked(input) }} />)
  }
     </div>
   )
