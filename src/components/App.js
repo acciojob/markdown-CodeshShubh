9445601a9edb93a0637e9c0549202d25109ed6ba
CodeshShubh <codeshshubh@gmail.com>
2025-04-02 10:57:20 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import MarkdownEditor from './MarkdownEditor';
 
 const App =  ()=>{
-    const [markdownInput , setmarkdownInput] = useState('# Heading\n\nThis is some **bold** text.')
+    const [markdownInput , setmarkdownInput] = useState('# Heading')
       return(
         <div style={{display:'flex'}} className='app'>
             <textarea style={{width:'50%',  height:'50vw' , resize:'none'}}
