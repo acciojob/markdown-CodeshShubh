1eac994c3ad0d60ca8a7bb4588ef7b080cd0c264
CodeshShubh <codeshshubh@gmail.com>
2025-04-02 10:06:24 +0530

    normalPreview2

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import MarkdownEditor from './MarkdownEditor';
 const App =  ()=>{
     const [markdownInput , setmarkdownInput] = useState('#Enter your Mark Down text here')
       return(
-        <div style={{display:'flex'}}>
-            <textarea style={{width:'50%',  height:'100vw'}}
+        <div style={{display:'flex'}} className='app'>
+            <textarea style={{width:'50%',  height:'50vw' , resize:'none'}}
             value={markdownInput}
             onChange={(e)=>setmarkdownInput(e.target.value)}
             >{markdownInput}</textarea>
--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const MarkdownEditor = ({markdownInput}) => {
+    const [input, setinput] = useState()
+    useEffect(()=>{
+       setinput(markdownInput)
+    })
   return (
-    <div style={{border:'1px solid red', width:'50%'}}>
-        {markdownInput}
+    <div style={{border:'1px solid red', width:'50%'}} className='preview'>
+        {input}
     </div>
   )
 }
